1ea187e5afbcac648e9c60c23dfeeb1e19bc2035
qzy <weelgod@gmail.com>
2017-07-07 08:28:33 +0800

    Lesson 5
    Lesson 5

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@
 import React, {Component} from "react";
 import {AppRegistry,} from "react-native";
 // import realAnimation from './Monment.main'
-import realAnimation from './Hearts.main'
+// import realAnimation from './Hearts.main'
+import realAnimation from './FAB'
 
 AppRegistry.registerComponent('EggHeadRNAnimations', () => realAnimation);
